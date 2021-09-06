767ea421e81ec451a176c45bb4fbb060bfc3eda9
Brandon <brandongua04@gmail.com>
2021-09-06 11:47:25 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     name='tml',
     version="1.0.0",
-    url='https://www.github.com/euro20179/tml',
+    url='https://www.github.com/brandongua/tml',
     description='The interpreter for the terminal markup language',
-    author='Euro',
+    author='Brandon',
     packages=['tml'],
     entry_points="""
         [console_scripts]
